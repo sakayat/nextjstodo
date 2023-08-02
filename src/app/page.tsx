e4d10e5dc73b4b56806cfd9efeadb10d26bae7cd
sakhyat <sakhyatkabir1@gmail.com>
2023-08-02 16:18:44 +0600

    fetch data from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,37 @@
+"use client";
 import style from "@/app/page.module.scss";
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export default function Home() {
+interface Todo {
+  _id: number;
+  content: string;
+}
+
+const HomePage: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // fetchData
+  const fetchData = async () => {
+    const res = await axios.get("/api/todos");
+    setTodos(res.data);
+  };
+
   return (
     <div className={style.wrapper}>
       <h1 className={style.title}>Todo App</h1>
-      <div className="">
+      <div>
         <TaskForm />
-        <TaskList />
+        <TaskList todos={todos} />
       </div>
     </div>
   );
-}
+};
+
+export default HomePage;
